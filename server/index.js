448a4d6f73cd99cc96525c84ea7f3c3e1105794e
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2021-10-03 00:40:47 +0300

    vuecrud

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+//import router from './router/router'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+dotenv.config()
+
+//app.use('/posts', router)
+app.listen(process.env.PORT, ()=>{
+    mongoose.connect(process.env.mongo_url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(console.log('connected to db'))
+    .catch((err)=> console.log(err))
+})
