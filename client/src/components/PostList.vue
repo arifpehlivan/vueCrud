f3b7511f9bf389331c22390e99f101e7db893f38
[arif pehlivan ] <[arifpehlivanapap@gmail.com]>
2021-10-04 22:28:56 +0300

    vue

---
+++ client/src/components/PostList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <div class="card my-5">
+  
+  <div class="card-content">
+    <div class="media">
+      
+      <div class="media-content">
+        <p class="title is-4">Başlık</p>
+        
+      </div>
+    </div>
+
+    <div class="content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+      
+      <p/>
+      <strong>creator</strong>
+    </div>
+  </div>
+</div>
+    </div>
+  
+</template>
+
+<script>
+export default {
+    setup(){
+        const posts = ref([])
+        const API_URL = 'http://localhost:5000/posts'
+
+        function getPosts
+    }
+}
+</script>
+
+<style>
+
+</style>
